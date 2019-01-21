283041241df2b2c742bb3a61ab5e283a4f04c2ac
Shawn.J.Tseng (tw mis.twtc01.Newegg) 61878 <st7j@buyabs.corp>
2019-01-21 13:39:39 +0800

    錯誤處理

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var x = document.getElementById('demo');
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
   } else {
     x.innerHTML = 'not supported';
   }
@@ -11,4 +11,21 @@ function getLocation() {
 function showPosition(position) {
   x.innerHTML = '緯度:' + position.coords.latitude +
       '<br>經度:' + position.coords.longitude;
+}
+
+function showError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = 'Be denied';
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = 'location is unavailable';
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = 'request timed out';
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = 'unknown error';
+      break;
+  }
 }

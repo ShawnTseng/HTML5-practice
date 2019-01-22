cc3c86e2fc8e8d24f51430e6927ef6008824e6e3
Shawn.J.Tseng (tw mis.twtc01.Newegg) 61878 <st7j@buyabs.corp>
2019-01-22 14:30:10 +0800

    Step3. 建立Web Worker

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+var w;
 function startWorker() {
   if (typeof (Worker) !== 'undefined') {
-    console.log("可以使用web worker");
+    console.log('可以使用web worker');
+    if (typeof (w) == 'undefined') {
+      console.log('如果Web Worker還沒建立才建立');
+      w = new Worker('demo_workers.js');
+      w.onmessage = function(event) {
+        document.getElementById('result').innerHTML = event.data;
+      };
+    }
   } else {
-    console.log("瀏覽器不支援web worker");
+    console.log('瀏覽器不支援web worker');
   }
 }
